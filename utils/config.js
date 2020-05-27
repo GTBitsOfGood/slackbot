05ba1201333f7b3fe809a7ea17e5332039003c61
Nicholas Boshart <nichabosh.dev@gmail.com>
2020-05-27 11:49:01 -0500

    Starting Fresh
    • Used previous work as a means of understanding the Slack API.
    • Deleted all files & directories associated with current endpoints.
       -> pages/api/**/*.js
       -> server/actions/*.js
       -> server/models/*.js
       -> utils/blocks.js
       -> utils/errors.js

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,5 @@
 export default {
   dbUrl: process.env.DB_URL,
   dbName: process.env.DB_NAME,
-  api: {
-    "start-meeting": "/api/admin/start-meeting",
-    "end-meeting": "/api/admin/end-meeting",
-    "register": "/api/user/register",
-    "checkin": "/api/user/checkin"
-  }
+  api: {}
 };
