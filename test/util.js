2129428e1c6f686db31dc905630c2ff5abd88f0b
Max Karpawich <maxkarpawich@gmail.com>
2020-06-06 17:35:51 -0400

    update deprecated Model.remove Mongoose call

--- test/util.js
+++ test/util.js
@@ -3,7 +3,7 @@ import connection from "../server/connection";
 async function clearModels(...models) {
   await connection();
   models.forEach(async (Model) => {
-    await Model.remove({});
+    await Model.deleteMany({});
   });
 }
 
