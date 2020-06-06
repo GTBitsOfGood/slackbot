e92230ad4cbdc104c8a8a02f2aedf8cbe7619d43
Max Karpawich <maxkarpawich@gmail.com>
2020-06-06 01:27:00 -0400

    remove the name field from the dummy command

--- server/commands/bog-beta.js
+++ server/commands/bog-beta.js
@@ -2,10 +2,8 @@ import { Command } from "../models/command";
 import Role from "../models/role";
 
 export default new Command({
-  name: "bog-beta",
   desc: "Abcdef",
   permissionLevel: Role.MEMBER
 }, (req, res) => {
-  console.log(req.args);
   res.nextRes.status(200).send("Hello, World!");
 });
