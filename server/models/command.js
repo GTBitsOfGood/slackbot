3b01661fb06d0d9bb6868d41e401a5d9ea92ae24
Max Karpawich <maxkarpawich@gmail.com>
2020-06-05 21:36:50 -0400

    remove the Argument model for commands in favor of flexibility

--- server/models/command.js
+++ server/models/command.js
@@ -1,20 +1,6 @@
 import Role from "./role";
 
 /**
- * @class
- * @classdesc A command argument.
- * @param {string} name The name of the command argument.
- * @param {boolean} [isOptional=false] Whether the command argument is optional; defaults to false.
- *
- * @throws {TypeError}
- */
-function Argument(name, isOptional = false) {
-  if (!name || typeof name !== "string") throw new TypeError("command arguments must have a name");
-  this.name = name;
-  this.isOptional = isOptional;
-}
-
-/**
  * The handler function for a command; see {@link Command}.
  * @callback handler
  * @param {Request} req The command request.
@@ -27,8 +13,6 @@ function Argument(name, isOptional = false) {
  * @param {Object} options The options object.
  * @param {string} options.name The name of the command.
  * @param {desc} options.desc The description of the command.
- * @param {Argument[]} [options.args=[]] The expected command arguments; defaults to [].
- * @param {number} [options.minArgs=0] The minimum number of arguments; defaults to 0.
  * @param {Role} options.permissionLevel The permission level of the command;
  *  see {@link Role} for possible values. The permission level of a command
  *  is hierarchial: for example, if the value is Role.MEMBER, then members with
@@ -46,14 +30,8 @@ function Command(options, handler) {
   this.name = options.name;
   if (!options.desc || typeof options.desc !== "string") throw new TypeError("commands must have a description");
   this.desc = options.desc;
-  this.args = options.args || [];
-  if (!Array.isArray(this.args)) throw new TypeError("options.args must be an Array");
-  this.args.forEach((arg) => {
-    if (!(arg instanceof Argument)) throw new TypeError("the options.args Array for a Command must only contain objects of type Argument.");
-  });
-  this.minArgs = options.minArgs || 0;
-  if (typeof this.minArgs !== "number" || this.minArgs < 0 || !Number.isInteger(this.minArgs)) throw new TypeError("options.minArgs must be a positive integer");
-  if (this.minArgs > this.args.length) throw new Error("the minimum number of arguments cannot exceed the length of the options.args Array");
+  if (typeof options.args !== "string") throw new TypeError("commands must have an args string");
+  this.args = options.args;
   if (typeof options.permissionLevel !== "string" || !Role.isRole(options.permissionLevel)) throw new TypeError("options.permissionLevel must be a valid role string. See the Role enum.");
   this.permissionLevel = options.permissionLevel;
   if (typeof handler !== "function") throw new TypeError("the handler argument must be a function");
@@ -65,7 +43,7 @@ function Command(options, handler) {
  * @classdesc A command request.
  * @param {Object} options The options object.
  * @param {Object} options.member The member who called the command; see the Member model.
- * @param {String[]} [options.args=[]] The command arguments passed in.
+ * @param {string} options.args The arguments string after the command.
  *
  * @throws {TypeError}
  */
@@ -93,5 +71,5 @@ function Response(res) {
 // TODO add Response helper functions
 
 export {
-  Argument, Command, Request, Response
+  Command, Request, Response
 };
