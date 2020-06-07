2cf04af4945523957a69ebc7f4faa7d94721c952
Max Karpawich <maxkarpawich@gmail.com>
2020-06-06 20:58:11 -0400

    replace Travis envvars w/ config backups

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 if (process.env.NODE_ENV !== "production") config();
 
 export default {
-  dbUrl: process.env.DB_URL,
-  dbName: process.env.DB_NAME,
+  dbUrl: process.env.DB_URL || "mongodb://localhost:27017/bog-bot",
+  dbName: process.env.DB_NAME || "bog-bot",
   api: {}
 };
