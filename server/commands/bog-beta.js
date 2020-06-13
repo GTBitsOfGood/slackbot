5f3f2bf07d30b1d769e830b6bc6e5f1d4e0e26dd
Max Karpawich <max.karpawich@gmail.com>
2020-06-13 02:11:01 -0400

    tmp: test out the Command role field pt. 2

--- server/commands/bog-beta.js
+++ server/commands/bog-beta.js
@@ -1,9 +1,9 @@
 import { Command } from "../models/command";
-// import { Role } from "../models/member";
+import { Role } from "../models/member";
 
 export default new Command({
   desc: "Abcdef",
-  roles: false
+  roles: Role.EXEC
 }, (req, res) => {
   res.nextRes.status(200).send("Hello, World!");
 });
