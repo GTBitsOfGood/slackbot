efa95378991e193270fc1543c2196d6bcad25d62
Max Karpawich <max.karpawich@gmail.com>
2020-06-13 02:02:57 -0400

    chore: resolve ESLint warning

--- server/connection.js
+++ server/connection.js
@@ -17,7 +17,7 @@ export default async (dbName, dbUrl) => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
-      useCreateIndex: true,
+      useCreateIndex: true
     })
     .catch((error) => {
       debug("Database connection failed. ↓");
